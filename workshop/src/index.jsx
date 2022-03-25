ac4a4d155535b9e9362d0657b7ab969ed6875c66
Tom Tsui <tomtsui2642@gmail.com>
2022-03-25 21:57:25 +0900

    basic react syntax

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
 import '../assets/stylesheets/application.scss';
+
+const root = document.getElementById('root');
+
+// rsc
+const Hello = (props) => {
+  const { name, age } = props;
+  const msg = `Hello ${name} and I'm ${age}`;
+  return <h1>{msg}</h1>;
+};
+
+// rcc
+// class Hello extends React.Component {
+//   render() {
+//     const { name, age } = this.props;
+//     const msg = `Hello ${name} and I'm ${age}`;
+//     return <h1>{msg}</h1>;
+//   }
+// }
+
+ReactDom.render(<Hello name="Ryan" age="10" />, root);
